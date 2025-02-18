4f6be1d1376e0964095d2f089fff81abdc80accf
PredoH1 <pedroh2006candido@gmail.com>
2025-02-18 17:48:29 -0300

    Feat: implemented catalog services

--- src/Components/Catalogo/index.jsx
+++ src/Components/Catalogo/index.jsx
@@ -1,5 +1,15 @@
 import style from "../../Components/Catalogo/Catalogo.module.css";
 import React, { useEffect, useRef, forwardRef } from "react";
+import catClassico from "../../assets/catClassico.jpg";
+import catEfeitoRimel from "../../assets/catEfeitoRimel.jpg";
+import catFoxBrasileiro from "../../assets/catFoxBrasileiro.jpg";
+import catFox4d from "../../assets/catFox4d.jpg";
+import catVolumeBrasileiro from "../../assets/catVolumeBrasileiro.jpg";
+import catVolume3d from "../../assets/catVolume3d.jpg";
+import catVolume4d from "../../assets/catVolume4d.jpg";
+import catVolume5d from "../../assets/catVolume5d.jpg";
+import catVolume6d from "../../assets/catVolume6d.jpg";
+import catEfeitoMolhado from "../../assets/catefeitoMolhado.jpg";
 
 function Catalogo(props, ref) {
   return (
@@ -9,27 +19,53 @@ function Catalogo(props, ref) {
       </div>
       <div className={style.boxCatalogo}>
         <div className={style.service}>
-          <h1>Cilios 1</h1>
+          <h1>Clássico</h1>
+          <img className={style.imgCat} src={catClassico} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Cilios 2</h1>
+          <h1>Efeito Rímel</h1>
+          <img className={style.imgCat} src={catEfeitoRimel} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Cilios 3</h1>
+          <h1>Fox Brasileiro</h1>
+          <img className={style.imgCat} src={catFoxBrasileiro} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Cilios 3</h1>
+          <h1>Fox 4D</h1>
+          <img className={style.imgCat} src={catFox4d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Cilios 3</h1>
+          <h1>Volume Brasileiro</h1>
+          <img className={style.imgCat} src={catVolumeBrasileiro} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Cilios 3</h1>
+          <h1>Volume 3D</h1>
+          <img className={style.imgCat} src={catVolume3d} alt="" />
+        </div>
+
+        <div className={style.service}>
+          <h1>Volume 4D</h1>
+          <img className={style.imgCat} src={catVolume4d} alt="" />
+        </div>
+
+        <div className={style.service}>
+          <h1>Volume 5D</h1>
+          <img className={style.imgCat} src={catVolume5d} alt="" />
+        </div>
+
+        <div className={style.service}>
+          <h1>Volume 6D</h1>
+          <img className={style.imgCat} src={catVolume6d} alt="" />
+        </div>
+
+        <div className={style.service}>
+          <h1>Efeito Molhado</h1>
+          <img className={style.imgCat} src={catEfeitoMolhado} alt="" />
         </div>
       </div>
     </section>
