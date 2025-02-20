1a79fe6d1e96fc5203f8a5d3fe6c5beb59c78ae7
PredoH1 <pedroh2006candido@gmail.com>
2025-02-20 17:38:37 -0300

    Style: improvement in design

--- src/Components/Catalogo/index.jsx
+++ src/Components/Catalogo/index.jsx
@@ -10,6 +10,7 @@ import catVolume4d from "../../assets/catVolume4d.jpg";
 import catVolume5d from "../../assets/catVolume5d.jpg";
 import catVolume6d from "../../assets/catVolume6d.jpg";
 import catEfeitoMolhado from "../../assets/catEfeitoMolhado.jpg";
+import iconCatalogo from "../../assets/iconCatalogo.svg";
 
 function Catalogo(props, ref) {
   return (
@@ -19,52 +20,91 @@ function Catalogo(props, ref) {
       </div>
       <div className={style.boxCatalogo}>
         <div className={style.service}>
-          <h1>Clássico</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Clássico</h1>
+          </div>
+
           <img className={style.imgCat} src={catClassico} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Efeito Rímel</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Efeito Rímel</h1>
+          </div>
+
           <img className={style.imgCat} src={catEfeitoRimel} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Fox Brasileiro</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Fox Brasileiro</h1>
+          </div>
+
           <img className={style.imgCat} src={catFoxBrasileiro} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Fox 4D</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Fox 4D</h1>
+          </div>
           <img className={style.imgCat} src={catFox4d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Volume Brasileiro</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Volume Brasileiro</h1>
+          </div>
+
           <img className={style.imgCat} src={catVolumeBrasileiro} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Volume 3D</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Volume 3D</h1>
+          </div>
+
           <img className={style.imgCat} src={catVolume3d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Volume 4D</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Volume 4D</h1>
+          </div>
+
           <img className={style.imgCat} src={catVolume4d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Volume 5D</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Volume 5D</h1>
+          </div>
+
           <img className={style.imgCat} src={catVolume5d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Volume 6D</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Volume 6D</h1>
+          </div>
+
           <img className={style.imgCat} src={catVolume6d} alt="" />
         </div>
 
         <div className={style.service}>
-          <h1>Efeito Molhado</h1>
+          <div className={style.descCat}>
+            <img src={iconCatalogo} alt="" />
+            <h1>Efeito Molhado</h1>
+          </div>
+
           <img className={style.imgCat} src={catEfeitoMolhado} alt="" />
         </div>
       </div>
